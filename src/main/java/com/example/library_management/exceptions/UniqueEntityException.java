6a1f86c44978931630efc935cce2d7862f7c5ed3
Shreishta Manoj <shreishtamanoj04@gmail.com>
2025-05-25 15:14:39 +0530

    Updated Controller

---
+++ src/main/java/com/example/library_management/exceptions/UniqueEntityException.java
@@ -0,0 +1,7 @@
+package com.example.library_management.exceptions;
+
+public class UniqueEntityException extends Exception{
+    public UniqueEntityException(String message){
+        super(message);
+    }
+}
